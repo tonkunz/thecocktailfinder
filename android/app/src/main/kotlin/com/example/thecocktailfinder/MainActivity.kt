89c8d945add56564eeef507dd9a3ba8bb1ed4b10
Everton Kunz <ehokeverton@gmail.com>
2021-02-01 20:19:22 -0400

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/example/thecocktailfinder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thecocktailfinder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:thecocktailfinder/pages/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Cocktail Finder',
+      debugShowCheckedModeBanner: false,
+      // Tema da aplicação, pode futuramente ir para um arquivo separado
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          brightness: Brightness.light,
+          primaryColor: Color(0xff583d72),
+          accentColor: Color(0xffff8e71)),
+      home: HomePage(),
+    );
+  }
+}
